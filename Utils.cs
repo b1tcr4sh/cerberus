3621e92f824a18bf18941b2abc59108bd71863d5
Kyra <ethan.ford06@gmail.com>
2023-04-03 10:20:02 -0500

    Guess I'm writing my own api :shrug:

--- Utils.cs
+++ Utils.cs
@@ -11,6 +11,55 @@ namespace Cerberus {
         public string Username { get; set; }
         public string Password { get; set; }
         public bool UsingOtp { get; set; }
-        // public string OtpCode = String.Empty;
+    }
+    public class LoginResponse
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string displayName { get; set; }
+        public string bio { get; set; }
+        public object[] bioLinks { get; set; }
+        public object[] pastDisplayNames { get; set; }
+        public bool hasEmail { get; set; }
+        public bool hasPendingEmail { get; set; }
+        public string email { get; set; }
+        public string obfuscatedEmail { get; set; }
+        public string obfuscatedPendingEmail { get; set; }
+        public bool emailVerified { get; set; }
+        public bool hasBirthday { get; set; }
+        public bool unsubscribe { get; set; }
+        public object[] friends { get; set; }
+        public object[] friendGroupNames { get; set; }
+        public string currentAvatarImageUrl { get; set; }
+        public string currentAvatarThumbnailImageUrl { get; set; }
+        public string currentAvatar { get; set; }
+        public string currentAvatarAssetUrl { get; set; }
+        public object accountDeletionDate { get; set; }
+        public int acceptedTOSVersion { get; set; }
+        public string steamId { get; set; }
+        public object steamDetails { get; set; }
+        public string oculusId { get; set; }
+        public bool hasLoggedInFromClient { get; set; }
+        public string homeLocation { get; set; }
+        public bool twoFactorAuthEnabled { get; set; }
+        public object feature { get; set; }
+        public string status { get; set; }
+        public string statusDescription { get; set; }
+        public string state { get; set; }
+        public object[] tags { get; set; }
+        public string developerType { get; set; }
+        public DateTime last_login { get; set; }
+        public string last_platform { get; set; }
+        public bool allowAvatarCopying { get; set; }
+        public bool isFriend { get; set; }
+        public string friendKey { get; set; }
+        public object[] onlineFriends { get; set; }
+        public object[] activeFriends { get; set; }
+        public object[] offlineFriends { get; set; }
+    }
+    public struct AuthTokens {
+        public string token { get; set; }
+        public bool using2FA { get; set; }
+        public string twoFactorToken { get; set; }
     }
 }
--- VRChatUtils.cs
+++ VRChatUtils.cs
@@ -1,4 +1,9 @@
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Web;
 using VRChat.API.Api;
 using VRChat.API.Model;
 using VRChat.API.Client;
@@ -9,14 +14,20 @@ namespace Cerberus {
         private VrchatLoginCredentials _credentials;
         private ILogger _logger;
         private bool _authed = false; 
+        private AuthenticationApi _authApi;
+        private AuthTokens _tokens;
+
+        private HttpClient _http;
         public VRChatAPI(VrchatLoginCredentials credentials, ILogger<VRChatAPI> logger) {
             _credentials = credentials;
             _logger = logger;
 
             Dictionary<String, String> apiKeys = new Dictionary<string, string>();
-            apiKeys.Add("apiKey", "JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26");
+            apiKeys.Add("auth", "JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26");
+            apiKeys.Add("twoFactorAuth", "JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26");
 
             configuration = new Configuration {
+                BasePath = "https://api.vrchat.cloud/api/1",
                 ApiKey = apiKeys,
                 Username = credentials.Username,
                 Password = credentials.Password,
@@ -30,45 +41,63 @@ namespace Cerberus {
             return await sysApi.GetCurrentOnlineUsersAsync();
         }
         public async Task<bool> Auth() {
-            AuthenticationApi authenticationApi = new AuthenticationApi(configuration);
-            CurrentUser user;
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = new CookieContainer();
+            HttpClient http = new HttpClient(handler);
 
-            try {
-                user = await authenticationApi.GetCurrentUserAsync();
-            } catch (ApiException e) {
-                _logger.LogWarning("Failed to login to VRChat: " + e.ErrorCode);
-                _logger.LogWarning(e.Message);
-                _authed = false;
+            // base64(urlencode(username):urlencode(password))
+            string encodedUsername = HttpUtility.UrlEncode(_credentials.Username);
+            string encodedPassword = HttpUtility.UrlEncode(_credentials.Password);
+            string base64Encoded = Base64Encode(String.Format("{0}:{1}", encodedUsername, encodedPassword));
+
+            http.DefaultRequestHeaders.Add("Authorization", "Basic " + base64Encoded);  
+            http.DefaultRequestHeaders.Add("User-Agent", "Cerberus / v0.1");
+
+            HttpResponseMessage res = await http.GetAsync("https://api.vrchat.cloud/api/1/auth/user?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26");
+            if (res.StatusCode != HttpStatusCode.OK) {
+                _logger.LogWarning("Couldn't login to VRChat; code: " + res.StatusCode);
                 return false;
             }
 
-            if (user is null) {
-                Verify2FAResult result;
-                _logger.LogInformation("Good job, you have 2FA enabled. I'll need that please");
-                string otpCode = Console.ReadLine();
-                configuration.AccessToken = otpCode;
-
-                try {
-                    result = await authenticationApi.Verify2FAAsync();
-                } catch (ApiException e) {
-                    _logger.LogWarning("Failed to login to VRChat: " + e.ErrorCode);
-                    _logger.LogWarning(e.Message);
-                    return false;
-                }
-
-                if (result.Verified) {
-                    _authed = true;
-                    _logger.LogInformation("Logged into vrchat!");
-                    return true;
-                } else {
-                    _authed = false;
-                    return false;
-                }
+            LoginResponse json = JsonSerializer.Deserialize<LoginResponse>(res.Content.ReadAsStringAsync().Result);
+            if (json.id is not null) {
+                string authCookie = handler.CookieContainer.GetCookies(new Uri("https://api.vrchat.cloud")).First().Value;
+                _tokens = new AuthTokens { token = authCookie, using2FA = false };
+                _logger.LogInformation("Logged into VRChat");
+                return true;
             }
 
-            _logger.LogInformation("Logged into vrchat!");
-            _authed = true;
+            string otp = RequestOtpLogin();
+
+            http.DefaultRequestHeaders.Remove("Authorization");
+            IDictionary<string, string> body = new Dictionary<string, string>();
+            body.Add("code", otp);
+            FormUrlEncodedContent content = new FormUrlEncodedContent(body);
+
+            HttpResponseMessage postRes = await http.PostAsync("https://api.vrchat.cloud/api/1/auth/twofactorauth/totp/verify?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26", content);
+            if (postRes.StatusCode != HttpStatusCode.OK) {
+                _logger.LogWarning("Couldn't login to VRChat; code: " + res.StatusCode);
+                return false;
+            }
+
+            CookieCollection cookies = handler.CookieContainer.GetCookies(new Uri("https://api.vrchat.cloud"));
+            string authToken = cookies.Where<Cookie>(cookie => cookie.Name.Equals("auth")).First().Value;
+            string twoFactorToken = cookies.Where<Cookie>(cookie => cookie.Name.Equals("twoFactorAuth")).First().Value;
+
+            _tokens = new AuthTokens { token = authToken, twoFactorToken = twoFactorToken, using2FA = true };
+            _logger.LogInformation("Logged into VRChat with 2FA");
             return true;
         }
+
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        private static string RequestOtpLogin() {
+            Console.Write("Your account has 2FA enabled (Good job <3), enter you OTP here > ");
+            return Console.ReadLine();
+        } 
     }
 }
